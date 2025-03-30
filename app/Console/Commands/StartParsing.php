a06149da748ad5e70cfefb28d2a7c37e5dd07fc8
Pavel <stalk6@gmail.com>
2025-03-30 17:13:35 +0300

    Create and adjust console command and parser config file

---
+++ app/Console/Commands/StartParsing.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class StartParsing extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:start-parsing';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Start parsing and writing to database';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+
+    }
+}
---
+++ config/parser.php
@@ -0,0 +1,17 @@
+<?php
+
+
+return [
+    'bot_token' => '5161694418:AAHZu6CIUxjlJXxF8LMz1kOY0pxlpDnKY7g',
+
+    'tg_subscribers' => [
+        '5192051989',
+        '295981983',
+    ],
+    'queries' => [
+        'orel_all' => ['title' => 'Поиск по всем ноутам', 'sity' => 'Орел', 'uri' => 'https://www.avito.ru/orlovskaya_oblast/noutbuki?f=ASgCAQECAUDwvA0UiNI0AUXGmgwYeyJmcm9tIjo3MDAwLCJ0byI6MjAwMDB9&s=104'],
+        'bryansk_all' => ['title' => 'Поиск по всем ноутам', 'sity' => 'Брянск', 'uri' => 'https://www.avito.ru/bryanskaya_oblast/noutbuki?f=ASgCAQECAUDwvA0UiNI0AUXGmgwYeyJmcm9tIjo3MDAwLCJ0byI6MjAwMDB9&s=104'],
+
+    ]
+
+];
