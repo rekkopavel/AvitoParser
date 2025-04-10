f5625a84ec148b1fce22a125400a1705d1e583dc
Pavel <stalk6@gmail.com>
2025-04-10 16:13:44 +0300

    Make refactoting HtmlParser

--- app/Services/Parser/HtmlServices/HtmlParser.php
+++ app/Services/Parser/HtmlServices/HtmlParser.php
@@ -10,8 +10,8 @@ use DOMXPath;
 class HtmlParser
 {
     private const NEW_PRODUCTS_REG_EXP = '/(>\d минут(ы|у)? назад<|>2 часа назад<|>1 час<|>Вчера<)/';
-    private const ALL_PRODUCTS_XPATH_EXP = '//div[@data-marker="item"]';
-    private const LINK_AND_TITLE_XPATH_EXP = '//a[@data-marker="item-title"]';
+    private const ALL_PRODUCTS_XPATH = '//div[@data-marker="item"]';
+    private const LINK_AND_TITLE_XPATH = '//a[@data-marker="item-title"]';
     private const AVITO_BASE_URL = 'https://www.avito.ru/';
 
 
@@ -21,45 +21,58 @@ class HtmlParser
 
     public function getProductsFromPage(string $html): array
     {
-        $doc = $this->createDomDocument($html);
-        $xpath = new DOMXPath($doc);
 
-        $allProductNodes= $xpath->query(Self::ALL_PRODUCTS_XPATH_EXP);
+        $allProductDivs = $this->parseAllProducts($html);
+        $newProductsDivs = $this->filterNewProducts($allProductDivs);
 
-        $allProductDivs = [];
-        foreach ($allProductNodes as $productNode) {
-            $allProductDivs[] = $doc->saveHTML($productNode);
-        }
+        return $this->extractProductsDataArray($newProductsDivs);
 
 
-        $newProductDivs= array_filter($allProductDivs, function ($html) {
-            $res =  preg_match(Self::NEW_PRODUCTS_REG_EXP, $html);
-            return $res;
-        });
+    }
 
-        $newProductDivs = array_values($newProductDivs); // сбросить ключи
-
-
-        $productsArray = [];
-        foreach ($newProductDivs as $div) {
+    private function extractProductsDataArray(array $productsDivs): array
+    {
+        $productsDataArray = [];
+        foreach ($productsDivs as $div) {
             $doc = $this->createDomDocument($div);
             $xpath = new DOMXPath($doc);
 
 
-            $linkNode = $xpath->query(Self::LINK_AND_TITLE_XPATH_EXP)->item(0);
+            $linkNode = $xpath->query(Self::LINK_AND_TITLE_XPATH)->item(0);
 
             if ($linkNode) {
                 $url = $linkNode->getAttribute('href');
                 $title = $linkNode->getAttribute('title');
 
-                $productsArray[] = [
-                    'uri' => Self::AVITO_BASE_URL.$url,
+                $productsDataArray[] = [
+                    'uri' => Self::AVITO_BASE_URL . $url,
                     'title' => $title,
                 ];
             }
         }
+        return $productsDataArray;
+    }
 
-        return $productsArray;
+    private function filterNewProducts(array $allProductDivs): array
+    {
+        return array_values(array_filter(
+            $allProductDivs,
+            fn($html) => preg_match(self::NEW_PRODUCTS_REG_EXP, $html)
+        ));
+    }
+
+    private function parseAllProducts(string $html): array
+    {
+        $doc = $this->createDomDocument($html);
+        $xpath = new DOMXPath($doc);
+
+        $allProductNodes = $xpath->query(self::ALL_PRODUCTS_XPATH);
+
+        $allProductDivs = [];
+        foreach ($allProductNodes as $productNode) {
+            $allProductDivs[] = $doc->saveHTML($productNode);
+        }
+        return $allProductDivs;
     }
 
     private function createDomDocument(string $html): DOMDocument
